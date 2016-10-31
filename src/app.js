d96ebfd5aa1c244e808b7c6695b1ccb58cb401bb
Alexander Lyakh <clapton@inbox.ru>
2016-10-31 17:33:18 +0300

    Updated app.js for use Graph class and renamed Graph component.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Graph from './components/graph';
+import GraphComponent from './components/graph';
+import Graph from './graph';
 import nodes from './db';
 
-ReactDOM.render(<Graph nodes={nodes}/>, document.getElementById('app'));
+const graph = new Graph();
+graph.insertNode(1, 'node1');
+graph.insertNode(2, 'node2');
+graph.insertNode(2, 'node3');
+graph.insertNode(2, 'node4');
+graph.insertNode(1, 'node5');
+
+console.log(graph);
+
+ReactDOM.render(<GraphComponent graph={graph} />, document.getElementById('app'));
